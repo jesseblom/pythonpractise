0f9d387ed28a31b5eabf8f4b5c246dda88a57080
jesseblom <jesse.blom@outlook.com>
2024-09-04 11:28:51 +0200

    building api

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return "Joehoe world"
+
+@app.route("/get-user/<user_id>")
+def get_user(user_id):
+    user_data = {
+        "user_id": user_id,
+        "name": "Joe Hoe",
+        "email": "joehoe@joehoe.com"
+    }
+
+    extra = request.args.get("extra")
+    if extra:
+        user_data["extra"] = extra
+
+    return jsonify(user_data), 200  # 200 is the status code
+
+@app.route("/create-user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+
+    return jsonify(data), 201
+
+if __name__ == '__main__':
+    app.run(debug=True) 
