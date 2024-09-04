e9a2e522a1cf8ca1a24774f25949928c54199f7d
jesseblom <jesse.blom@outlook.com>
2024-09-04 17:21:25 +0200

    saving work

--- Flask Rest API tutorial/test.py
+++ Flask Rest API tutorial/test.py
@@ -2,8 +2,15 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/3", {"likes": 10, "name": "Spongebob", "views": 1337})
-print(response.json())
+
+data = [{"likes": 721, "name": "Spongebob the movie", "views": 12341432},
+        {"likes": 1415, "name": "How to make REST API", "views": 10132400},
+        {"likes": 1312312312321, "name": "Shrek", "views": 1039393939393}]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
 input()
-response = requests.get(BASE + "video/6")
+response = requests.patch(BASE + "video/2", {"views": 99, "likes": 190213231231311})
 print(response.json())
